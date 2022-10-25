50a06493e1ab418639bd4f98e2acb4e9c74f042f
AlvinJS <manzi1alvin@gmail.com>
2022-10-25 22:49:15 +0200

    refresher continued

--- index.js
+++ index.js
@@ -11,3 +11,30 @@ const activeJob = jobs.filter(job => job.isActive);
 
 console.log(activeJobs);
 console.log(activeJob);
+
+
+class Person {
+    constructor(name){
+    this.name = name;
+    }
+    walk(){
+        console.log("walk");
+    }
+}
+
+class Teacher extends Person{
+    constructor (name,degree) {
+        super(name);
+        this.degree = degree;
+    }
+    teach(){
+        console.log("teach");
+    }
+}
+
+
+const teacher = new Teacher ("Mosh","MSc");
+console.log (teacher);
+teacher.walk();
+teacher.teach();
+
